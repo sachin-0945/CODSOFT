3e01580d859a5da602306a886d46bdddc57fc373
Sachin <138795801+sachin-0945@users.noreply.github.com>
2023-11-27 14:05:32 +0530

    Delete Python Tasks directory

--- Python Tasks/Password generator using python.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-length = int(input("Enter the length of the password you want to generate: "))
-password=[]
-characters = string.ascii_letters + string.digits + string.punctuation
-for i in range(length):
-    randomchar = random.choice(characters)
-    password.append(randomchar)
-print("Generated Password: ", ''.join(password))
